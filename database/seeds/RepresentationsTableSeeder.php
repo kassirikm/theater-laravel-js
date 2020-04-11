8221d3ce232aa91f98e1324fea68639792018486
Ali Dindar <ali_dindar@live.be>
2020-04-12 01:13:04 +0200

    representations seeder

--- database/seeds/RepresentationsTableSeeder.php
+++ database/seeds/RepresentationsTableSeeder.php
@@ -11,6 +11,22 @@ class RepresentationsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('representations')->insert([
+            'when' => '2020-06-21 20:00:00',
+            'show_id' => '1',
+            'location_id' => '1',
+        ]);
+
+        DB::table('representations')->insert([
+            'when' => '2020-07-01 19:00:00',
+            'show_id' => '2',
+            'location_id' => '3',
+        ]);
+
+        DB::table('representations')->insert([
+            'when' => '2020-08-15 21:00:00',
+            'show_id' => '3',
+            'location_id' => '2',
+        ]);
     }
 }
