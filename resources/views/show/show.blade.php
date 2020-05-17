20abd8a11a80d4bac73efd4c8d61471b03e13f42
ArthurDOTmpeg <Arthur.a379@gmail.com>
2020-05-17 19:38:38 +0200

    Show template : amélioration liste des artistes

--- resources/views/show/show.blade.php
+++ resources/views/show/show.blade.php
@@ -36,14 +36,26 @@
         @endif
         
         <h2>Liste des artistes</h2>
-        <ul>
-            @foreach ($show->artistTypes as $collaborateur)
-                <li>
-                    {{ $collaborateur->artist->firstname }}
-                    {{ $collaborateur->artist->lastname }}
-                    ({{ $collaborateur->type->type }})
-                </li>              
-            @endforeach
-        </ul>
+        <p><strong>Auteur:</strong>
+        @foreach ($collaborateurs['auteur'] as $auteur)
+            {{ $auteur->firstname }} 
+            {{ $auteur->lastname }}@if($loop->iteration == $loop->count-1) et 
+            @elseif(!$loop->last), @endif
+        @endforeach
+        </p>
+        <p><strong>Metteur en scène:</strong>
+        @foreach ($collaborateurs['scénographe'] as $scenographe)
+            {{ $scenographe->firstname }} 
+            {{ $scenographe->lastname }}@if($loop->iteration == $loop->count-1) et 
+            @elseif(!$loop->last), @endif
+        @endforeach
+        </p>
+        <p><strong>Distribution:</strong>
+        @foreach ($collaborateurs['comédien'] as $comedien)
+            {{ $comedien->firstname }} 
+            {{ $comedien->lastname }}@if($loop->iteration == $loop->count-1) et 
+            @elseif(!$loop->last), @endif
+        @endforeach
+        </p>
 
 @endsection
