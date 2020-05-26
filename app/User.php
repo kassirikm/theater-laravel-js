0a96a93f1b7475ed3e0170bd12e52d840da1a4a5
ArthurDOTmpeg <Arthur.a379@gmail.com>
2020-05-26 09:45:58 +0200

    Add Billable

--- app/User.php
+++ app/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
+
 
 class User extends \TCG\Voyager\Models\User implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable, Billable;
 
     /**
      * The attributes that are mass assignable.
