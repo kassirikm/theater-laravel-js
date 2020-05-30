605c171442421f65db470259a0cf4949ad926764
ArthurDOTmpeg <Arthur.a379@gmail.com>
2020-05-30 12:01:09 +0200

    Amélioration page spectacle

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -5,30 +5,35 @@
 @section('content')
 
     <div class="container">
-         <h2>Filterable Table</h2>
-         <p>Type something in the input field to search and filter the table</p>
+         <h2>Rechercher un spectacle</h2>
          <input class="form-control" id="myInput" type="text" placeholder="Search..">
          <br>
       <table class="table table-bordered table-striped">
           <h1>Liste des {{ $resource }}</h1>
           <thead>
           <tr>
-            <th>Show</th>
-            <th>Déscription</th>
-            <th>bookable</th>
+            <th>Spectacle</th>
+            <th>Description</th>
             <th>Représentations</th>
             <th>Prix</th>
             <th>Réserver</th>
           </tr>
           </thead>
         <tbody id="show">
-       @foreach($shows as $row)
+       @foreach($shows as $show)
        <tr>
-        <td>{{$row['title']}}</td>
-        <td>{{$row['description']}}</td>
-        <td>{{$row['bookable']}}</td>
-        <td>{{$row['representations']}}</td>
-        <td>{{$row['price']}}€</td>
+        <td>{{$show->title}}</td>
+        <td>{{$show['description']}}</td>
+        <td>
+            @if($show->representations->count()==1)
+            <span>1 représentation</span>
+            @elseif($show->representations->count()>1)
+            <span>{{ $show->representations->count() }} représentations</span>
+            @else
+            <em>aucune représentation</em>
+            @endif
+        </td>
+        <td>{{$show['price']}}€</td>
         <!-- {{ route('checkout.index') }} : aller vers la page de paiement -->
         <!-- action('ShowController@edit', $row['id']) -->
         <td><a href="{{ route('checkout.index') }}" class="btn btn-warning">Réserver</a></td>
