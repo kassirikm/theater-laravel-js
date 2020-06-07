2cfaf3792bf0a54fe2ce9d1776f017021c4949c2
Ali Dindar <ali_dindar@live.be>
2020-06-07 06:59:37 +0200

    try to solve the problem to deploy with a composer update

--- vendor/tcg/voyager/migrations/2016_01_01_000000_add_voyager_user_fields.php
+++ vendor/tcg/voyager/migrations/2016_01_01_000000_add_voyager_user_fields.php
@@ -13,6 +13,7 @@ class AddVoyagerUserFields extends Migration
             if (!Schema::hasColumn('users', 'avatar')) {
                 $table->string('avatar')->nullable()->after('email')->default('users/default.png');
             }
+            $table->bigInteger('role_id')->nullable()->after('id');
         });
     }
 
