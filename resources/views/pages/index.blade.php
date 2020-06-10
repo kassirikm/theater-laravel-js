3ac3e9aea3c4f05d5506f931891192e4015e430f
Ali Dindar <ali_dindar@live.be>
2020-06-10 17:56:31 +0200

    code refactoring home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,10 +4,7 @@
 
     <!-- Theater main image -->
 <br><br>
-<div class="jumbotron jumbotron-fluid"
-     <!-- style="background-image: url(http://d1wf8hd6ovssje.cloudfront.net/styles/960x263-banner/s3/images/National_Theatre_banner_3.jpg); -->
-    style="background-image: url(National_Theatre_banner.jpg);
-     background-size:100%;">
+<div class="jumbotron jumbotron-fluid" style="background-image: url('images/National_Theatre_banner.jpg');background-size:100%;">
     <div class="container">
         <h1 class="display-4" style="text-center; color:white;">Théatre National</h1>
         <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
@@ -35,35 +32,4 @@
 </div>
 
 </body>
-
-    <!--
-    <div class="card-deck">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-    </div>
-    -->
-
-
 @endsection
